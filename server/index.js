78e74d32fec5d78bcb5983b33da2af959bc65613
min-hinthar <102896221+min-hinthar@users.noreply.github.com>
2023-03-08 17:56:53 -0800

    openai configuration with apiKey imported into server/index.js, openai.js created for router.post(/text, async(req, res) and exported as router

--- server/index.js
+++ server/index.js
@@ -4,6 +4,9 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import helmet from 'helmet';
 import morgan from 'morgan';
+import { Configuration, OpenAIApi } from "openai";
+
+import openAiRoutes from './routes/openai.js';
 
 // CONFIGURATIONS
 dotenv.config();
@@ -16,6 +19,15 @@ app.use(morgan("common"));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
+
+// OPENAI CONFIGURATION
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+export const openai = new OpenAIApi(configuration);
+
+// ROUTES
+app.use('/openai', openAiRoutes)
 
 // SERVER SETUP
 const PORT = process.env.PORT || 9000;
