c1d6fbb4b96bdf4f5d7f3f52097ec5dfe4e8d879
min-hinthar <102896221+min-hinthar@users.noreply.github.com>
2023-03-08 16:27:20 -0800

    components/chat/index.jsx updated with MultiChat imports and chatProps to render customHeader,customHeader created with index.jsx and updated with chat.title and chat.description with Heroicons

--- client/src/components/chat/index.jsx
+++ client/src/components/chat/index.jsx
@@ -1,8 +1,27 @@
 import React from 'react';
+import { 
+    useMultiChatLogic, 
+    MultiChatSocket, 
+    MultiChatWindow 
+} from 'react-chat-engine-advanced';
+import Header from '@/components/customHeader';
 
 const Chat = () => {
+    const chatProps = useMultiChatLogic(
+        import.meta.env.VITE_PROJECT_ID,
+        "Admin",
+        "abcd"
+    );
+
   return (
-    <div>Chat</div>
+    <div style={{ flexBasis: '100%' }}>
+        <MultiChatSocket {...chatProps} />
+        <MultiChatWindow 
+            {...chatProps}
+            style={{ height: '100vh' }}
+            renderChatHeader={(chat) => <Header chat={chat} />}
+        />
+    </div>
   )
 }
 
