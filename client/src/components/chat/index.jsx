f6f3ebb766d6508f5356c5f0469eb2c96bc207f0
min-hinthar <102896221+min-hinthar@users.noreply.github.com>
2023-03-09 13:21:40 -0800

    server/npm i openai, client/chat/index.jsx updated conditional rendering startWith OpenAI_, Ai.jsx updated with OpenAI trigger api endpoint usePostAiTextMutation(form)

--- client/src/components/chat/index.jsx
+++ client/src/components/chat/index.jsx
@@ -7,6 +7,7 @@ import {
 
 import Header from '@/components/customHeader';
 import StandardMessageForm from '@/components/customMessageForms/StandardMessageForm';
+import Ai from '@/components/customMessageForms/Ai';
 
 const Chat = () => {
     const chatProps = useMultiChatLogic(
@@ -15,21 +16,25 @@ const Chat = () => {
         "abcd"
     );
 
-  return (
-    <div style={{ flexBasis: '100%' }}>
-        <MultiChatSocket {...chatProps} />
-        <MultiChatWindow 
-            {...chatProps}
-            style={{ height: '100vh' }}
-            renderChatHeader={(chat) => <Header chat={chat} />}
-            renderMessageForm={(props) => {
-                return (
-                    <StandardMessageForm props={props} activeChat={chatProps.chat} />
-                )
-            }}
-        />
-    </div>
-  )
-}
+    return (
+        <div style={{ flexBasis: '100%' }}>
+            <MultiChatSocket {...chatProps} />
+            <MultiChatWindow 
+                {...chatProps}
+                style={{ height: '100vh' }}
+                renderChatHeader={(chat) => <Header chat={chat} />}
+                renderMessageForm={(props) => {
+                    if (chatProps.chat?.title.startsWith("OpenAI_")) {
+                        return <Ai props={props} activeChat={chatProps.chat} />
+                    }
 
-export default Chat
+                    return (
+                        <StandardMessageForm props={props} activeChat={chatProps.chat} />
+                    )
+                }}
+            />
+        </div>
+    )
+};
+
+export default Chat;
--- client/src/components/customMessageForms/Ai.jsx
+++ client/src/components/customMessageForms/Ai.jsx
@@ -1,3 +1,4 @@
+import { usePostAiTextMutation } from '@/state/api';
 import React, { useState } from 'react';
 
 import MessageFormUI from './MessageFormUI';
@@ -5,23 +6,27 @@ import MessageFormUI from './MessageFormUI';
 const Ai = ({ props, activeChat }) => {
   const [message, setMessage] = useState("");
   const [attachment, setAttachment] = useState("");
+  // Use Post Mutation OPEN AI API Endpoint
+  const [ trigger ] = usePostAiTextMutation();
 
   const handleChange = (e) => setMessage(e.target.value);
   const handleSubmit = async () => {
       const date = new Date()
-          .toISOString()
-          .replace("T", " ")
-          .replace("Z", `${Math.floor(Math.random() * 1000)}+00:00`);
+        .toISOString()
+        .replace("T", " ")
+        .replace("Z", `${Math.floor(Math.random() * 1000)}+00:00`);
       const at = attachment ? [{ blob: attachment, file: attachment.name }] : [];
       const form = {
-          attachments: at,
-          created: date,
-          sender_username: props.username,
-          text: message,
-          activeChatId: activeChat.id,
+        attachments: at,
+        created: date,
+        sender_username: props.username,
+        text: message,
+        activeChatId: activeChat.id,
       }
       
       await props.onSubmit(form);
+      // OPEN AI API CALL Triggered
+      trigger(form);
       setMessage("");
       setAttachment("");
   };
@@ -36,4 +41,4 @@ const Ai = ({ props, activeChat }) => {
   )
 }
 
-export default Ai
+export default Ai;
